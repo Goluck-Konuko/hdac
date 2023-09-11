8b67571dd94a8f281147448b795014b78de0a825
Goluck <goluck25@gmail.com>
2023-09-11 09:59:48 +0200

    Repo  migration with updates

--- modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .components import *
-from .dac import DAC, HDAC
-from .generator_model import GeneratorFullModel
-from .dicriminator_model import DiscriminatorFullModel
--- modules/components/__init__.py
+++
@@ -1,3 +0,0 @@
-from .discriminator import MultiScaleDiscriminator
-from .kp_detector import KPD
-from .generator import GeneratorDAC, GeneratorHDAC
